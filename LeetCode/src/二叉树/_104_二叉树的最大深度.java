412d1f4f8414196d51d3811cb50e993143462ce0
sy <2350199075@qq.com>
2020-06-09 10:55:34 +0800

    104

--- LeetCode/src/二叉树/_104_二叉树的最大深度.java
+++ LeetCode/src/二叉树/_104_二叉树的最大深度.java
@@ -1,5 +1,10 @@
 package 二叉树;
 
 public class _104_二叉树的最大深度 {
-
+	public int maxDepth(TreeNode root) {
+		if (root == null) return 0;
+		int leftMaxDepth = maxDepth(root.left);
+		int rightMaxDepth = maxDepth(root.right);
+		return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
 }
